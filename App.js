bb776cc9d04c5a8096eb407010bcab5d17f1df47
Efe Mert Şahinkoç <study.efemertsahinkoc@outlook.com>
2024-02-23 22:11:35 +0300

    request

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>bu bir saldırıdır.!</Text>
+      <Text>Bizim repoyu private yap babacan!</Text>
       <StatusBar style="auto" />
     </View>
   );
