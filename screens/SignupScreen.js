e3f05e9e772e4abcfa99c8edf40e529bb093a2cf
Efe Mert Şahinkoç <study.efemertsahinkoc@outlook.com>
2024-02-26 22:52:54 +0300

    databaseTestBranch

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -14,8 +14,40 @@ function SignupScreen() {
   async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
     try {
+      
+
+      registerUser = async(email,password)=>{
       const token = await createUser(email, password);
       authCtx.authenticate(token);
+        // Creating user with email and password
+        await firebase.auth().createUserWithEmailAndPassword(email,password).then(()=>{
+            // Sending email verification
+            firebase.auth().currentUser.sendEmailVerification({
+                handleCodeInApp: true,
+                url:'https://piclink-app.firebaseapp.com',
+            })
+            .then(() =>{
+                alert('Verification email sent') // Alerting user about verification email
+            }).catch((error) =>{
+                alert(error.message) // Alerting user if sending verification email fails
+            })
+            .then(()=>{
+                // Adding user details to Firestore after successful registration
+                firebase.firestore().collection('users')
+                .doc(firebase.auth().currentUser.uid)
+                .set({
+                    email
+                })
+            })
+            .catch((error)=>{
+                alert(error.message) // Alerting user if adding user details to Firestore fails
+            })
+        })
+        .catch((error)=>{
+            alert(error.message) // Alerting user if user registration fails
+        })
+    }
+
     } catch (error) {
       Alert.alert(
         'Authentication failed',
